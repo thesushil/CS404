8cc8351c9685a749f12568bbf55df6da5ba43d9b
Sushil Singh <thesushil@gmail.com>
2017-02-25 12:00:05 -0600

    completed 2C

--- src/com/sushil/PracExam2C.java
+++ src/com/sushil/PracExam2C.java
@@ -3,32 +3,93 @@ package com.sushil;
 public class PracExam2C {
     // Problem 1
     public static int combin_Rec(int n, int k) {
-        return 1;
+        if (n == 0 || k == 0 || n == k) return 1;
+        return combin_Rec(n - 1, k - 1) + combin_Rec(n - 1, k);
     }
 
     // Problem 2
-    public static int combin_NonRec(int n, int k) {return 1;}
+    public static int combin_NonRec(int n, int k) {
+        int[][] C = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            C[i][0] = 1;
+            C[0][i] = 1;
+            C[i][i] = 1;
+        }
+
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= k; j++) {
+                if (i != j) C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+            }
+
+        return C[n][k];
+    }
 
     // Problem 3
-    public static int combin_NonRecBetter(int n, int k) {return 1;}
+    public static int combin_NonRecBetter(int n, int k) {
+        int[][] C = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            C[i][0] = 1;
+            C[0][i] = 1;
+            C[i][i] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int jStart = (i - n + k <= 0 ? 1 : i - n + k);
+            int jEnd = (i < k ? i : k);
+            for (int j = jStart; j <= jEnd; j++) {
+                if (i != j) C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+            }
+        }
+        return C[n][k];
+    }
 
     // Problem 4
-    public static int numWays_NonRec(int amt, int k, int [] D) {return 1;}
+    public static int numWays_NonRec(int amt, int k, int[] D) {
+        if (amt < 0) return 0;
+        int[][] ways = new int[amt + 1][k + 1];
+        for (int i = 0; i <= k; i++) {
+            ways[0][i] = 1;
+        }
+        for (int i = 0; i <= amt; i++) {
+            ways[i][0] = 0;
+            if (k > 0) ways[i][1] = 1;
+        }
+
+        for (int i = 1; i <= amt; i++)
+            for (int j = 1; j <= k; j++) {
+                int amt1 = i - D[j];
+                ways[i][j] = (amt1 < 0 ? 0 : ways[amt1][j]) + ways[i][j - 1];
+            }
+
+        return ways[amt][k];
+    }
 
     // Problem 5
     public static int two_to_n_Rec(int n) {
-        if (n == 1) return 2; System.out.println(5555555);
+        if (n == 1) return 2;
+        System.out.println(5555555);
         return two_to_n_Rec(n - 1) + two_to_n_Rec(n - 1);
     }
 
     // Problem 6
     public static int two_to_n_Dyn(int n) {
-        int[] pow = new int[n+1];
+        int[] pow = new int[n + 1];
         pow[0] = 1;
-        for (int i = 1; i <= n; i++) pow[i] = pow[i-1] + pow[i-1];
+        for (int i = 1; i <= n; i++) pow[i] = pow[i - 1] + pow[i - 1];
         return pow[n];
     }
 
     // Problem 7
-    public static int trib(int n) {return 1;}
+    public static int trib(int n) {
+        if (n <= 0) return 0;
+        int[] t = new int[n + 1];
+        if (n >= 1) t[1] = 1;
+        if (n >= 2) t[2] = 2;
+        if (n >= 3) t[3] = 4;
+        for (int i = 4; i <= n; i++)
+            t[i] = t[i - 1] + t[i - 2] + t[i - 3];
+        return t[n];
+    }
 }
--- src/com/sushil/PracExam2CTest.java
+++ src/com/sushil/PracExam2CTest.java
@@ -4,28 +4,97 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by 212358644 on 2/22/2017.
- */
 class PracExam2CTest {
     @Test
     void combin_Rec() {
+        int res;
 
+        res = PracExam2C.combin_Rec(0, 10);
+        assertEquals(1, res);
+
+        res = PracExam2C.combin_Rec(10, 0);
+        assertEquals(1, res);
+
+        res = PracExam2C.combin_Rec(0, 0);
+        assertEquals(1, res);
+
+        res = PracExam2C.combin_NonRec(3, 2);
+        assertEquals(3, res);
+
+        res = PracExam2C.combin_Rec(10, 5);
+        assertEquals(252, res);
+
+        res = PracExam2C.combin_Rec(50, 7);
+        assertEquals(99884400, res);
     }
 
     @Test
     void combin_NonRec() {
+        int res;
 
+//        res = PracExam2C.combin_NonRec(0, 10);
+//        assertEquals(1, res);
+
+        res = PracExam2C.combin_NonRec(10, 0);
+        assertEquals(1, res);
+
+        res = PracExam2C.combin_NonRec(0, 0);
+        assertEquals(1, res);
+
+        res = PracExam2C.combin_NonRec(3, 2);
+        assertEquals(3, res);
+
+        res = PracExam2C.combin_NonRec(10, 5);
+        assertEquals(252, res);
+
+        res = PracExam2C.combin_NonRec(50, 7);
+        assertEquals(99884400, res);
     }
 
     @Test
     void combin_NonRecBetter() {
+        int res;
 
+//        res = PracExam2C.combin_NonRecBetter(0, 10);
+//        assertEquals(1, res);
+
+        res = PracExam2C.combin_NonRecBetter(10, 0);
+        assertEquals(1, res);
+
+        res = PracExam2C.combin_NonRecBetter(0, 0);
+        assertEquals(1, res);
+
+        res = PracExam2C.combin_NonRec(3, 2);
+        assertEquals(3, res);
+
+        res = PracExam2C.combin_NonRecBetter(10, 5);
+        assertEquals(252, res);
+
+        res = PracExam2C.combin_NonRecBetter(50, 7);
+        assertEquals(99884400, res);
     }
 
     @Test
     void numWays_NonRec() {
+        int ways;
 
+        ways = PracExam2C.numWays_NonRec(0, 0, new int[]{0});
+        assertEquals(0, ways);
+
+        ways = PracExam2C.numWays_NonRec(0, 3, new int[]{0, 1, 5, 25});
+        assertEquals(1, ways);
+
+        ways = PracExam2C.numWays_NonRec(32, 3, new int[]{0, 1, 5, 25});
+        assertEquals(9, ways);
+
+        ways = PracExam2C.numWays_NonRec(4, 3, new int[]{0, 1, 2, 3});
+        assertEquals(4, ways);
+
+        ways = PracExam2C.numWays_NonRec(5, 3, new int[]{0, 1, 2, 3});
+        assertEquals(5, ways);
+
+        ways = PracExam2C.numWays_NonRec(10, 4, new int[]{0, 2, 5, 3, 6});
+        assertEquals(5, ways);
     }
 
     @Test
@@ -64,7 +133,31 @@ class PracExam2CTest {
 
     @Test
     void trib() {
+        int res;
 
+        res = PracExam2C.trib(1);
+        assertEquals(1, res);
+
+        res = PracExam2C.trib(2);
+        assertEquals(2, res);
+
+        res = PracExam2C.trib(3);
+        assertEquals(4, res);
+
+        res = PracExam2C.trib(4);
+        assertEquals(7, res);
+
+        res = PracExam2C.trib(5);
+        assertEquals(13, res);
+
+        res = PracExam2C.trib(6);
+        assertEquals(24, res);
+
+        res = PracExam2C.trib(7);
+        assertEquals(44, res);
+
+        res = PracExam2C.trib(8);
+        assertEquals(81, res);
     }
 
 }
