6e9daf25ce8190a797151f43ad187fcd75366f45
Sushil Singh <thesushil@gmail.com>
2017-04-22 13:23:33 -0500

    Completed hasStar and primConnected

--- src/main/java/com/sushil/PracExam4C.java
+++ src/main/java/com/sushil/PracExam4C.java
@@ -1,13 +1,15 @@
 package com.sushil;
 
 public class PracExam4C {
+    public static final int INFINITE = 9999;
+
     public static int[] bruteForceOptimal(int n, int a, int b, int c) {
         int count = Integer.MAX_VALUE;
         int[] sol = new int[3];
 
-        for (int nc = 0; nc <= n; nc++)
-            for (int nb = 0; nb <= n; nb++)
-                for (int na = 0; na <= n; na++)
+        for (int nc = 0; nc <= n / c; nc++)
+            for (int nb = 0; nb <= n / b; nb++)
+                for (int na = 0; na <= n / a; na++)
                     if (na * a + nb * b + nc * c == n && na + nb + nc < count) {
                         sol = new int[]{na, nb, nc};
                         count = na + nb + nc;
@@ -15,4 +17,59 @@ public class PracExam4C {
 
         return sol;
     }
+
+    public static boolean hasStar(int[][] W) {
+        int n = W.length - 1;
+        for (int i = 1; i <= n; i++) {
+            boolean isStar = true;
+            for (int j = 1; j <= n; j++) {
+                if (W[i][j] == INFINITE) {
+                    isStar = false;
+                    break;
+                }
+            }
+            if (isStar) return true;
+        }
+        return false;
+    }
+
+    public static boolean primConnected(boolean[][] W) {
+        int n = W.length - 1, source = 1;
+        boolean[] distance = new boolean[n + 1];
+        boolean[] unexplored = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            distance[i] = W[source][i];
+            unexplored[i] = true;
+        }
+        unexplored[source] = false;
+
+        for (int loop = 1; loop < n; loop++) {
+            int vNear = 0;
+            for (int i = 1; i <= n; i++) {
+                if (distance[i] && unexplored[i]) {
+                    vNear = i;
+                    break;
+                }
+            }
+            if (vNear == 0) return false;
+            unexplored[vNear] = false;
+
+            for (int i = 1; i <= n; i++) {
+                if (W[i][vNear] && unexplored[i])
+                    distance[i] = W[i][vNear];
+            }
+        }
+
+        return true;
+    }
+
+    public static int numComponents(int [][] W){
+        int n = W.length;
+
+
+
+        return n;
+    }
+
 }
--- src/test/java/com/sushil/PracExam4CTest.java
+++ src/test/java/com/sushil/PracExam4CTest.java
@@ -5,11 +5,81 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PracExam4CTest {
+
     @Test
     public void bruteForceOptimal() throws Exception {
-        assertArrayEquals(new int[]{2,0,0}, PracExam4C.bruteForceOptimal(10, 5, 2, 1));
-        assertArrayEquals(new int[]{2,1,1}, PracExam4C.bruteForceOptimal(13, 5, 2, 1));
-        assertArrayEquals(new int[]{1,7,1}, PracExam4C.bruteForceOptimal(123, 25, 13, 7));
+        assertArrayEquals(new int[]{2, 0, 0}, PracExam4C.bruteForceOptimal(10, 5, 2, 1));
+        assertArrayEquals(new int[]{2, 1, 1}, PracExam4C.bruteForceOptimal(13, 5, 2, 1));
+        assertArrayEquals(new int[]{1, 7, 1}, PracExam4C.bruteForceOptimal(123, 25, 13, 7));
+    }
+
+    @Test
+    public void hasStar() throws Exception {
+        int INF = PracExam4C.INFINITE;
+        int[][] W;
+
+        W = new int[][]{
+                {0, 0, 0, 0},
+                {0, 0, 3, 4},
+                {0, 4, 0, 6},
+                {0, 4, 6, 0}};
+        assertTrue(PracExam4C.hasStar(W));
+
+        W = new int[][]{
+                {0, 0, 0, 0},
+                {0, 0, 3, 4},
+                {0, INF, 0, 6},
+                {0, 4, 6, 0}};
+        assertTrue(PracExam4C.hasStar(W));
+
+        W = new int[][]{
+                {0, 0, 0, 0},
+                {0, 0, 3, INF},
+                {0, INF, 0, 6},
+                {0, 4, 6, 0}};
+        assertTrue(PracExam4C.hasStar(W));
+
+        W = new int[][]{
+                {0, 0, 0, 0},
+                {0, 0, 3, INF},
+                {0, INF, 0, 6},
+                {0, 4, INF, 0}};
+        assertFalse(PracExam4C.hasStar(W));
+    }
+
+    @Test
+    public void primConnected() throws Exception{
+        boolean[][] W;
+
+        W = new boolean[][]{
+                {false, false, false, false},
+                {false, true, true, false},
+                {false, true, true, true},
+                {false, false, true, true}};
+        assertTrue(PracExam4C.primConnected(W));
+
+        W = new boolean[][]{
+                {false, false, false, false},
+                {false, true, true, false},
+                {false, true, true, false},
+                {false, false, false, true}};
+        assertFalse(PracExam4C.primConnected(W));
+
+        W = new boolean[][]{
+                {false, false, false, false,false},
+                {false, true, true, false,true},
+                {false, true, true, false,false},
+                {false, false, false, true,true},
+                {false, true, false, true, true}};
+        assertTrue(PracExam4C.primConnected(W));
+
+        W = new boolean[][]{
+                {false, false, false, false,false},
+                {false, true, true, false,true},
+                {false, true, true, false,false},
+                {false, false, false, true,false},
+                {false, true, false, false, true}};
+        assertFalse(PracExam4C.primConnected(W));
     }
 
 }
